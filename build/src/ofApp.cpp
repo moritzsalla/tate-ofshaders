52561d5cf8c94591336ab5c215d116be911911e8
Moritz Salla <moritz.salla@hotmail.de>
2020-03-01 17:50:07 +0000

    minor

--- build/src/ofApp.cpp
+++ build/src/ofApp.cpp
@@ -7,70 +7,70 @@ void ofApp::setup(){
     ofDirectory dir(shaderPath);
     dir.listDir();
     dirCount = dir.size();
-
-    ofSetFrameRate(24);
+    
+    ofSetFrameRate(60);
     setupWebcam();
     sphere.set(150, 40); // Radius, Resolution
+    
+    shader.load("5/shader");
 }
 
 //--------------------------------------------------------------
 void ofApp::update() {
-    //    shader.load("3/shader");
-
+    
     if(webcamInUse) webcam.update();
-
-
-    if (ofGetFrameNum()%100 == 0) {
-        shader.unload();
-        ofLog() << i;
-        setShader();
-
-        if (!shader.isLoaded()) {
-            i = 0;
-            setShader();
-        }
-        if(++i == dirCount) {
-            ofLog() << "Resetting i";
-            i = 0;
-        }
-    }
+    
+    //        if (ofGetFrameNum()%100 == 0) {
+    //            shader.unload();
+    //            ofLog() << i;
+    //            setShader();
+    //
+    //            if (!shader.isLoaded()) {
+    //                i = 0;
+    //                setShader();
+    //            }
+    //            if(++i == dirCount) {
+    //                ofLog() << "Resetting i";
+    //                i = 0;
+    //            }
+    //        }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
     ofSetColor(120);
-
+    
     if(webcamInUse){
         frame.begin();
         ofClear(255);
         frame.end();
         fbo.begin();
         ofClear(0, 0, 0, 255);
-    } 
+    }
     if(sphereInUse){
         ofEnableDepthTest();
         ofBackgroundGradient(ofColor(40), ofColor(0));
         cam.begin();
         ofPushMatrix();
     }
-
+    
     shader.begin();
     shader.setUniform1f("time", ofGetElapsedTimef());
     shader.setUniform2f("mouse", mouseX, mouseY);
     shader.setUniform2f("resolution", ofGetWidth(), ofGetHeight());
-
+    
     if(webcamInUse){
         shader.setUniformTexture("tex0", webcam.getTexture(), 1);
         frame.draw(0, 0);
     } else if(sphereInUse){
         sphere.draw();
-
+        
     } else {
         ofDrawRectangle(0, 0, ofGetWidth(), ofGetHeight());
     }
-
+    
     shader.end();
-
+    
     if(webcamInUse) {
         fbo.end();
         fbo.draw(0,0);
@@ -80,17 +80,17 @@ void ofApp::draw(){
         ofDisableDepthTest();
     }
 }
-  
+
 //--------------------------------------------------------------
 void ofApp::setShader () {
     string path = std::to_string(i) + "/shader";
-
+    
     if(i == 4) webcamInUse = true;
     if(i != 4) webcamInUse = false;
-
+    
     if (i == 5) sphereInUse = true;
     if(i != 5) sphereInUse = false;
-
+    
     shader.load(path);
 }
 
@@ -98,59 +98,58 @@ void ofApp::setShader () {
 void ofApp::setupWebcam(){
     camWidth = ofGetWidth();
     camHeight = ofGetHeight();
-
+    
     webcam.setVerbose(false);
     webcam.initGrabber(camWidth, camHeight);
-
+    
     fbo.allocate(camWidth, camHeight);
     frame.allocate(camWidth, camHeight);
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
     camWidth = w;
     camHeight = h;
-
 }
 
 //--------------------------------------------------------------
 void ofApp::gotMessage(ofMessage msg){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::dragEvent(ofDragInfo dragInfo){
-
+    
 }
 
